9dc8a1f3676824c5eb9cacf2c817feaa1f8c7214
HruthVik <hruthviknaik03@gmail.com>
2024-04-03 13:45:07 +0530

    Created and commited the main page of the clone

---
+++ gemini_clone/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from "../../assets/assets"
+
+
+import "./Sidebar.css"
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className="top">
+                <img src="" alt="" />
+            </div>
+            <div className="bottom">
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
