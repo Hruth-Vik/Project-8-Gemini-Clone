e0f0d01e7eaa0fff497aa104714ef5c3528b1c8e
HruthVik <hruthviknaik03@gmail.com>
2024-04-04 23:09:44 +0530

    edited the right part of the page

--- gemini_clone/src/components/Main/Main.jsx
+++ gemini_clone/src/components/Main/Main.jsx
@@ -8,6 +8,30 @@ const Main = () => {
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
             </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello,Dev.</span></p>
+                    <p>How can i help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, beatae!</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, beatae!</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
