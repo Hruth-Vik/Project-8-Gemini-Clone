5660461726e87bdddb32dfb6c84dfcff493d94f0
HruthVik <hruthviknaik03@gmail.com>
2024-04-05 08:46:50 +0530

    added the search bar styling

--- gemini_clone/src/components/Main/Main.jsx
+++ gemini_clone/src/components/Main/Main.jsx
@@ -35,9 +35,14 @@ const Main = () => {
                     <div className="search-box">
                         <input type="text" placeholder='Enter a prompt here' />
                         <div>
-
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
                         </div>
                     </div>
+                    <p className='bottom-info'>
+                        Gemini may display inaccurate info,including about people,so double check its responses.Your privacy and Gemini Apps.
+                    </p>
                 </div>
             </div>
 
