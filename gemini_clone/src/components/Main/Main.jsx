fafb8b4871b96e59589afb265267a377f2e18490
HruthVik <hruthviknaik03@gmail.com>
2024-04-08 00:20:24 +0530

    results have been generated in the console and adding the result into the page dynamically

--- gemini_clone/src/components/Main/Main.jsx
+++ gemini_clone/src/components/Main/Main.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Main.css";
 import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
+
+
 const Main = () => {
+    const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context);
     return (
         <div className='main'>
             <div className="nav">
@@ -9,35 +13,44 @@ const Main = () => {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello,Dev.</span></p>
-                    <p>How can i help you today?</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, beatae!</p>
-                        <img src={assets.compass_icon} alt="" />
+                {!showResult
+                    ?
+                    <>
+                        <div className="greet">
+                            <p><span>Hello,Dev.</span></p>
+                            <p>How can i help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, beatae!</p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <img src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <img src={assets.message_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, beatae!</p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <div className='result'>
+
                     </div>
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, beatae!</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
+                }
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder='Enter a prompt here' />
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            <img src={assets.send_icon} alt="" />
+                            <img onClick={() => onSent()} src={assets.send_icon} alt="" />
                         </div>
                     </div>
                     <p className='bottom-info'>
@@ -46,7 +59,7 @@ const Main = () => {
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
--- gemini_clone/src/context/Context.jsx
+++ gemini_clone/src/context/Context.jsx
@@ -1,18 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 
 const ContextProvider = (props) => {
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompt, setPrevPrompt] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
 
     const onSent = async (prompt) => {
-        await runChat(prompt);
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        const response = await runChat(input);
+        setResultData(response)
+        setLoading(false)
+        setInput("")
     }
-    onSent("what is react js");
 
     const contextValue = {
-
+        prevPrompt,
+        setPrevPrompt,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
     }
     return (
         <Context.Provider value={contextValue}>
@@ -21,4 +41,4 @@ const ContextProvider = (props) => {
     )
 }
 
-export default ContextProvider
+export default ContextProvider;
